7d0eaf70a4bf81b74e7a2dded7e2dd68375a9d20
kwonjonny <128343377+kwonjonny@users.noreply.github.com>
2023-09-16 03:44:49 +0900

    파이프라인 구축 코드변경 커밋
    파이프라인 구축 코드변경 커밋

--- application/src/main/java/docker/application/controller/DockerApplicationController.java
+++ application/src/main/java/docker/application/controller/DockerApplicationController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("spring/")
 public class DockerApplicationController {
-    
+
     @GetMapping("docker")
     public String dockerApplication() {
+        System.out.println("코드");
+        System.out.println("젠킨스 코드 변경 커밋!!!!");
+        System.out.println("코드");
         return "spring/docker";
     }
 }
