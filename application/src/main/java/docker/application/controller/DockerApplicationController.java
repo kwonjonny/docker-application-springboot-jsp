bfc28cabab68e77884c58ce65e8ca9c948a7d1a7
kwonjonny <128343377+kwonjonny@users.noreply.github.com>
2023-09-16 03:50:37 +0900

    코드 디코이
    코드 디코이

--- application/src/main/java/docker/application/controller/DockerApplicationController.java
+++ application/src/main/java/docker/application/controller/DockerApplicationController.java
@@ -4,15 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 @RequestMapping("spring/")
 public class DockerApplicationController {
 
     @GetMapping("docker")
     public String dockerApplication() {
-        System.out.println("코드");
-        System.out.println("젠킨스 코드 변경 커밋!!!!");
-        System.out.println("코드");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+        log.info("코드 변경 컨트롤러");
+
         return "spring/docker";
     }
 }
